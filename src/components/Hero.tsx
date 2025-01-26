6a6d7dc187907285b987550ae1952257ce61ac07
Leex <thomas@thirty3.de>
2025-01-26 17:43:58 +0100

    Initial commit from your app

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,12 +5,9 @@ import { useState } from 'react';
     import Newsletter from './Newsletter';
     import ComingSoonBadge from './ComingSoonBadge';
     import { Link } from 'react-router-dom';
-    import { siteConfig } from '../config';
 
     export default function Hero() {
       const [showNewsletter, setShowNewsletter] = useState(false);
-      const basePath = siteConfig.url.startsWith('http') ? new URL(siteConfig.url).pathname : siteConfig.url;
-      const normalizedBasePath = basePath.endsWith('/') ? basePath : `${basePath}/`;
 
       const footerItems = [
         {
@@ -48,7 +45,7 @@ import { useState } from 'react';
               
               <div className="flex flex-col sm:flex-row gap-4 justify-center">
                 <Link
-                  to={`${normalizedBasePath}resources`}
+                  to="/dist/resources"
                   className="px-8 py-4 bg-cyan-500 hover:bg-cyan-600 text-white rounded-lg font-semibold transition-all transform hover:scale-105"
                 >
                   Explore Resources
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { siteConfig } from './config';
-
-const basePath = siteConfig.url.startsWith('http') ? new URL(siteConfig.url).pathname : siteConfig.url;
-const normalizedBasePath = basePath.endsWith('/') ? basePath : `${basePath}/`;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={normalizedBasePath}>
+    <BrowserRouter basename="/dist/">
       <App />
     </BrowserRouter>
   </StrictMode>
